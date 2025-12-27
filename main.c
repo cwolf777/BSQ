53068d5c0e178b243ba3fdc658d67e94d080b78e
Christian Wolf <cwolf@3-F-3.42heilbronn.de>
2025-12-27 11:17:15 +0100

    adds possibility to read on standart input if argc == 1

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cwolf <cwolf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 13:10:38 by cwolf             #+#    #+#             */
-/*   Updated: 2025/12/16 16:16:40 by cwolf            ###   ########.fr       */
+/*   Updated: 2025/12/27 10:52:19 by cwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main (int argc, char *argv[])
 {
     FILE *file;
-
+    
     if (argc == 2)
     {
         file = fopen(argv[1], "r");
@@ -24,11 +24,21 @@ int main (int argc, char *argv[])
             fprintf(stderr, "map error\n");
             return -1;
         }
-        if(execute(file) == -1)
+        if (execute(file)  == -1)
         {
             fprintf(stderr, "map error\n");
             return -1;
         }
     }
+    else if (argc == 1)
+    {
+        file = stdin;
+        if (execute(file) == -1)
+        {
+            fprintf(stderr, "map error\n");
+        }
+    }
+    if (file)
+        fclose(file); 
     return 0;
 }
