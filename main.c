d30c0e32fed5daea85484c0843d58176b3e835c1
Christian Wolf <cwolf@3-A-7.42heilbronn.de>
2026-01-14 18:13:38 +0100

    fixed main ->must be able to read several maps

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cwolf <cwolf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 13:10:38 by cwolf             #+#    #+#             */
-/*   Updated: 2025/12/27 10:52:19 by cwolf            ###   ########.fr       */
+/*   Updated: 2026/01/14 18:12:53 by cwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,37 @@
 int main (int argc, char *argv[])
 {
     FILE *file;
-    
-    if (argc == 2)
+
+    if (argc == 1)
     {
-        file = fopen(argv[1], "r");
-        if (!file)
-        {
-            fprintf(stderr, "map error\n");
-            return -1;
-        }
-        if (execute(file)  == -1)
-        {
-            fprintf(stderr, "map error\n");
-            return -1;
-        }
-    }
-    else if (argc == 1)
-    {
-        file = stdin;
-        if (execute(file) == -1)
+        if (execute(stdin) == -1)
         {
             fprintf(stderr, "map error\n");
         }
     }
-    if (file)
-        fclose(file); 
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            file = fopen(argv[i], "r");
+            if (!file)
+            {
+                fprintf(stderr, "map error\n");
+            }
+            else
+            {
+                if (execute(file) == -1)
+                {
+                    fprintf(stderr, "map error\n");
+                }
+                fclose(file);
+            }
+            
+            if (i < argc - 1)
+            {
+                fprintf(stdout, "\n");
+            }
+        }
+    }
     return 0;
 }
